9ec0be3f37513b8f946389aa614196766b7371b9
Fajobi Anselm <pulumbu11@gmail.com>
2023-05-30 03:21:13 +0100

    new

---
+++ src/component/Home.js
@@ -0,0 +1,105 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../styles/home.css";
+
+const Home = () => {
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      const cursor = document.querySelector(".cursor");
+      cursor.style.left = event.clientX + "px";
+      cursor.style.top = event.clientY + "px";
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      const mainImg = document.querySelector(".main-img img");
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
+      const imgRect = mainImg.getBoundingClientRect();
+      const imgCenterX = imgRect.left + imgRect.width / 2;
+      const imgCenterY = imgRect.top + imgRect.height / 2;
+      const moveX = (mouseX - imgCenterX) / 10;
+      const moveY = (mouseY - imgCenterY) / 10;
+      mainImg.style.transition = "transform 0.2s ease-out";
+      mainImg.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    };
+
+    const mainImg = document.querySelector(".main-img img");
+    mainImg.addEventListener("mouseenter", () => {
+      document.addEventListener("mousemove", handleMouseMove);
+    });
+    mainImg.addEventListener("mouseleave", () => {
+      mainImg.style.transition = "transform 0.5s ease-in-out";
+      mainImg.style.transform = "translate(0, 0)";
+      document.removeEventListener("mousemove", handleMouseMove);
+    });
+
+    return () => {
+      mainImg.removeEventListener("mouseenter", () => {
+        document.addEventListener("mousemove", handleMouseMove);
+      });
+      mainImg.removeEventListener("mouseleave", () => {
+        mainImg.style.transition = "transform 0.5s ease-in-out";
+        mainImg.style.transform = "translate(0, 0)";
+        document.removeEventListener("mousemove", handleMouseMove);
+      });
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="container pt-5">
+        <div className="row d-flex justify-content-between">
+          <div className="col-lg-6 main-writing">
+            <p className="position-relative">
+              BERT IS A YELLOW MUPPET CHARACTER ON THE LONG RUNNING PBS AND HBO
+              CHILDREN'S TELEVISION SHOW SESAME STREET.
+              <span className="pseudo-element"></span>
+            </p>
+            <h2>BERTCOIN</h2>
+            <div className="buy-button pt-2 d-flex flex-column gap-20">
+              <button className="button ">
+                <span>BUY NOW</span>
+              </button>
+              <button className="button">BUY NOW</button>
+            </div>
+          </div>
+          <div className="col-lg-5 main-img">
+            <img
+              src="https://bertcoin.io/assets/img/bertcoin.png"
+              alt="bertcoin"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="slider-shape">
+        <img
+          src="https://bertcoin.io/assets/img/slider/slider_shape03.png"
+          alt="shape"
+        />
+        <img
+          src="https://bertcoin.io/assets/img/slider/slider_shape03.png"
+          alt="shape"
+        />
+        <img
+          src="https://bertcoin.io/assets/img/slider/slider_shape03.png"
+          alt="shape"
+        />
+        <img
+          src="https://bertcoin.io/assets/img/slider/slider_shape03.png"
+          alt="shape"
+        />
+      </div>
+      <div className="cursor"></div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/component/Navbar.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/Image 29-05-2023 at 10.18.JPG";
+import { FaBars } from "react-icons/fa";
+import { TfiWrite } from "react-icons/tfi";
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  const [showFabars, setShowFabars] = useState(false);
+  const [showFabars2, setShowFabars2] = useState(false);
+  const [showButtons, setShowButtons] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      setShowFabars(windowWidth <= 1000);
+      setShowFabars2(windowWidth >= 980);
+      setShowButtons(windowWidth >= 600);
+      setShowLinks(windowWidth >= 980);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <header className="pt-2 pb-3">
+      <div className="first-nav">
+        <div className="container navbar-container">
+          <div className="row">
+            <div className="col-12">
+              <div className="header-divide d-flex justify-content-between align-items-center">
+                <div className="w-logo d-flex gap-50 align-items-center">
+                  <img
+                    className="logo1"
+                    src="https://bertcoin.io/assets/img/bertcoin.png"
+                    alt="logo"
+                  />
+                  {showLinks && (
+                    <div className="a-links">
+                      <Link>Home</Link>
+                      <Link>About Us</Link>
+                      <Link>Contact</Link>
+                    </div>
+                  )}
+                </div>
+
+                <div className="button-bar d-flex justify-content-between align-items-center gap-45">
+                  <div className="d-flex gap-45 align-items-center">
+                    {showButtons && (
+                      <button className="nav-button d-flex justify-content-between align-items-center">
+                        <TfiWrite />
+                        <span>Buy Now</span>
+                      </button>
+                    )}
+                    {showFabars2 && (
+                      <div className="bars">
+                        <FaBars className="text-white fs-2" />
+                      </div>
+                    )}
+                  </div>
+                  {showFabars && (
+                    <div className="mobile-bars">
+                      <FaBars className="text-white fs-2 fabars-m" />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
